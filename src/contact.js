7383c83962b5ea0f1ea79b2f9541275ad09cb373
Dominic Oludare <dominicoludare@gmail.com>
2020-11-12 11:29:38 +0100

    Contact Form Validation

--- src/contact.js
+++ src/contact.js
@@ -16,26 +16,27 @@ class Contact extends React.Component {
                     <p className="render-inline"><span>&lt;</span>h1<span>&gt;</span></p>
                     <h2 className="contact-me">Contact me</h2>
                     <p className="render-inline"><span>&lt;</span>/h1<span>&gt;</span></p>
-                    <p className="what-i-do">I am a Junior web developer who is open to freelance opportunities and, 
-                        part-time/full-time Job. However, if you have other request or 
+                    <p className="what-i-do">I am a Web Developer who is open to freelance opportunities , 
+                        part-time and full-time Job. I write mostly React  However, if you have other request or 
                         response, don't hesitate to contact me using below form either.
                     </p>
                 </div>
 
-                <form className="entry">
-                    <input type="text" className="names" name="name" placeholder="Name"/><br/><br/>
-                    <input type="email" className="names" name="email" placeholder="Email"/><br/><br/>
-                    <input type="text" className="names" name="subject" placeholder="Subject"/><br/><br/>
-                    <textarea className="message" name="message" placeholder="Message" /><br/><br/>
-                    <button className="send"><span className="send-span">Send</span>
+                <form name="contact" method="post" action="/contact" className="entry">
+                    <input type="hidden" name="form-name" value="contact" />
+                    <input type="text" className="names" name="name" placeholder="Name" required/><br/><br/>
+                    <input type="email" className="names" name="email" placeholder="Email" required/><br/><br/>
+                    <input type="text" className="names" name="subject" placeholder="Subject" required/><br/><br/>
+                    <textarea className="message" name="message" placeholder="Message" required/><br/><br/>
+                    <button type="submit" className="send"><span className="send-span">Send</span>
                     </button>
                     <br/>
                 </form>
                 <div className="details">
-                    <p>Dominic Oludare,</p>
-                    <p>Lagos, Nigeria.</p>
-                    <p><span style={{color: "cyan"}}>@</span>: dominicoludare@gmail.com</p>
-                    <span>
+                    <p className="details-props">Dominic Oludare,</p>
+                    <p className="details-props">Lagos, Nigeria.</p>
+                    <p className="details-props"><span style={{color: "cyan"}}>@</span>: dominicoludare@gmail.com</p>
+                    <span className="details-props">
                         <FontAwesomeIcon style={{color: "cyan"}} icon={faPhone} size="1x"/>
                         {" "}
                         <span>+234-7052065184</span>
