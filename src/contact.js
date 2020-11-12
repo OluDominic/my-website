2ec5eaabfb79a87201e474c9bab18795c0d10ed7
Dominic Oludare <dominicoludare@gmail.com>
2020-11-12 12:06:13 +0100

    form valid

--- src/contact.js
+++ src/contact.js
@@ -22,12 +22,12 @@ class Contact extends React.Component {
                     </p>
                 </div>
 
-                <form name="contact" method="post" action="/contact" className="entry">
-                    <input type="text" className="names" name="name" placeholder="Name" required/><br/><br/>
-                    <input type="email" className="names" name="email" placeholder="Email" required/><br/><br/>
-                    <input type="text" className="names" name="subject" placeholder="Subject" required/><br/><br/>
-                    <textarea className="message" name="message" placeholder="Message" required/><br/><br/>
-                    <button type="submit" className="send"><span className="send-span">Send</span>
+                <form className="entry">
+                    <input type="text" className="names" name="name" placeholder="Name"/><br/><br/>
+                    <input type="email" className="names" name="email" placeholder="Email"/><br/><br/>
+                    <input type="text" className="names" name="subject" placeholder="Subject"/><br/><br/>
+                    <textarea className="message" name="message" placeholder="Message" /><br/><br/>
+                    <button className="send"><span className="send-span">Send</span>
                     </button>
                     <br/>
                 </form>
