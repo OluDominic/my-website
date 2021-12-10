a15a833adcb9fac02acbf48144540f6ef77b1208
Dominic Oludare <dominicoludare@gmail.com>
2021-12-10 16:09:44 +0100

    Add new project to portfolio

--- src/portfolio.js
+++ src/portfolio.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './portfolio.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
-import { faClone } from '@fortawesome/free-solid-svg-icons'
+import { faClone, faLink } from '@fortawesome/free-solid-svg-icons'
 import {Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 
@@ -47,73 +47,73 @@ class Portfolio extends React.Component {
             <h1 className="header">My Projects</h1>
             <div className="portfolio">
 
+                <div className="project2">
+                <span className="icons">
+                    <Link to={{pathname: "https://github.com/OluDominic/dog-ecommerce"}} target="_blank" className="icons">
+                        <FontAwesomeIcon icon={faGithub} size="2x" color="gray" className="ecommerce"/>
+                    </Link>
+                </span>
+                <span>
+                    <Link to={{pathname: "https://dog-commerce.herokuapp.com/"}} target="_blank">
+                        <FontAwesomeIcon icon={faLink} size="2x" color="gray" />
+                    </Link>
+                </span>
+                    <p className="siwes">Doggypup <br/> E-commerce App.</p>
+                    <p className="languages">REST API/HTML/SCSS/REACT/REDUX</p>
+                </div>
+
                 <div className="projectFour">
                     <span className="icons">
-                    <a href="https://github.com/OluDominic/tas-salary-system">
+                    <Link to={{pathname: "https://github.com/OluDominic/tas-salary-system"}} target="_blank">
                         <span>
                         <FontAwesomeIcon icon={faGithub} size="2x" color="gray" />
                         </span>
-                        </a>
+                        </Link>
                     </span>
                     
                     <span className="icons">
-                        <Link onClick={this.handleClickFour}>
-                        <FontAwesomeIcon icon={faClone} size="2x" color="gray" />
+                        <Link to={{pathname: "https://hr-management-app1.herokuapp.com/"}} target="_blank">
+                        <FontAwesomeIcon icon={faLink} size="2x" color="gray" />
                         </Link>
                         </span>
                     <p className="siwes">HR Management <br/> App.</p>
                     <p className="languages">REACT/SASS/NODEJS/MYSql</p>
                 </div>
 
-                <div className="project">
+                <div className="projectFive">
                     <span className="icons">
-                    <a href="https://github.com/OluDominic/siwes/tree/master/src">
+                    <Link to={{pathname: "https://github.com/OluDominic/frontend-onboarding"}} target="_blank">
                         <span>
                         <FontAwesomeIcon icon={faGithub} size="2x" color="gray" />
                         </span>
-                        </a>
+                        </Link>
                     </span>
                     
                     <span className="icons">
-                        <Link onClick={this.handleClick}>
-                        <FontAwesomeIcon icon={faClone} size="2x" color="gray" />
+                        <Link to={{pathname: "https://shielded-citadel-81661.herokuapp.com/"}} target="_blank">
+                        <FontAwesomeIcon icon={faLink} size="2x" color="gray" />
+                        </Link>
+                        </span>
+                    <p className="siwes">Food Order <br/> App.</p>
+                    <p className="languages">REACT/SASS</p>
+                </div>
+
+                <div className="project">
+                    <span className="icons">
+                    <Link to={{pathname: "https://github.com/OluDominic/siwes/tree/master/src"}} target="_blank">
+                        <span>
+                        <FontAwesomeIcon icon={faGithub} size="2x" color="gray" />
+                        </span>
+                        </Link>
+                    </span>
+                    
+                    <span className="icons">
+                        <Link to={{pathname: "https://siwes-tech.herokuapp.com/"}} target="_blank">
+                        <FontAwesomeIcon icon={faLink} size="2x" color="gray" />
                         </Link>
                         </span>
                     <p className="siwes">E-Siwes <br/> App.</p>
-                    <p className="languages">HTML/CSS REACT</p>
-                </div>
-
-                <div className="projectFive">
-                    <span className="icons">
-                    <a href="https://github.com/OluDominic/tac-pay">
-                        <span>
-                        <FontAwesomeIcon icon={faGithub} size="2x" color="gray" />
-                        </span>
-                        </a>
-                    </span>
-                    
-                    <span className="icons">
-                        <Link onClick={this.handleClickFive}>
-                        <FontAwesomeIcon icon={faClone} size="2x" color="gray" />
-                        </Link>
-                        </span>
-                    <p className="siwes">ID Payment <br/> App.</p>
-                    <p className="languages">REACT/SASS/NODEJS/MYSql</p>
-                </div>
-
-                <div className="project2">
-                <span className="icons">
-                    <a href="https://github.com/OluDominic/dog-ecommerce" className="icons">
-                        <FontAwesomeIcon icon={faGithub} size="2x" color="gray" className="fagit"/>
-                    </a>
-                </span>
-                <span>
-                    <Link onClick={this.handleClickTwo}>
-                        <FontAwesomeIcon icon={faClone} size="2x" color="gray" />
-                    </Link>
-                </span>
-                    <p className="siwes">Doggypup <br/> E-commerce App.</p>
-                    <p className="languages">API HTML/CSS REACT REDUX</p>
+                    <p className="languages">HTML/CSS/REACT</p>
                 </div>
 
                 {/* <div className="project3">
